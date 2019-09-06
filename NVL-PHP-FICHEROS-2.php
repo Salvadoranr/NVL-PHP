041f5698f2b23fd766ea3080fbb9d582940a8a5c
salvadoranr <salvadoranr16@gmail.com>
2019-09-06 15:06:11 +0200

    ficheros 2 ruta

--- NVL-PHP-FICHEROS-2.php
+++ NVL-PHP-FICHEROS-2.php
@@ -2,8 +2,8 @@
 reemplazar('el quijote.txt', 'quijote-modificado.txt', 'Sancho', 'Morty');
 function reemplazar($file, $newFile, $name, $newName)
 {
-    $descriptor = fopen("C:\Users\Salvador\Desktop\bootcamp\NVL-PHP/$file", 'r');
-    $descriptorM = fopen("C:\Users\Salvador\Desktop\bootcamp\NVL-PHP/$newFile", 'w');
+    $descriptor = fopen("./$file", 'r');
+    $descriptorM = fopen("./$newFile", 'w');
   
     while (($line = fgets($descriptor)) !== false) {
         $newPhrase =  str_replace($name, $newName, $line);
