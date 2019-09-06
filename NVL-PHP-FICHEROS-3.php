611c3c917f4bf19a81347ec70b42635565f2eb6c
salvadoranr <salvadoranr16@gmail.com>
2019-09-06 16:07:50 +0200

    fichero 3

--- NVL-PHP-FICHEROS-3.php
+++ NVL-PHP-FICHEROS-3.php
@@ -1,9 +1,9 @@
 <?php
-$algo = date('Y-m-d G i');
-mkdir($algo);
+$newDir = date('Y-m-d G i');
+mkdir($newDir);
 
  $fichero = './el quijote.txt';
- $nuevo_fichero = "./$algo/el quijote.txt.modificado";
+ $nuevo_fichero = "./$newDir/el quijote.txt.modificado";
  copy($fichero, $nuevo_fichero);
 
 ?>
